7c363d6f9522559eaa62c35bef4ddd6c782ea4a5
BetulCengiz <bc199911@hotmail.com>
2024-03-09 23:35:42 +0300

    Add project files.

---
+++ Intro/DataAccess/Abstracts/ICourseDal.cs
@@ -0,0 +1,16 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.DataAccess.Abstracts;
+
+public interface ICourseDal //interface,şablon
+
+{// ICourseDal'ı implemente eden sınıf bu metotları içermek zorunda 
+    List<Course> GetAll();
+    void Add(Course course);
+
+}
---
+++ Intro/Entities/BaseCustomer.cs
@@ -0,0 +1,9 @@
+﻿namespace Intro.Entities;
+
+public class BaseCustomer
+{
+    // buradaki özellikler  ortaktır ve diğer classlara buradan miras verilir
+    public int Id { get; set; }
+    public string CustomerNumber { get; set; }
+
+}
